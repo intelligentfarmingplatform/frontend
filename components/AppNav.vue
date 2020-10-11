2bb7e534ace9c9622cd00bbdce288a1059ecbfc8
intelligent.farming.platform@gmail.com <intelligent.farming.platform@gmail.com>
2020-10-12 05:03:59 +0700

    add register page & update login / middleware

--- components/AppNav.vue
+++ components/AppNav.vue
@@ -1,53 +1,67 @@
 <template>
   <div>
-  <b-navbar toggleable="lg" type="dark" variant="info">
-    <b-navbar-brand href="#">Intelligent Farming</b-navbar-brand>
+    <b-navbar toggleable="lg" type="dark" variant="info">
+      <b-navbar-brand href="#">Intelligent Farming</b-navbar-brand>
 
-    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
-    <b-collapse id="nav-collapse" is-nav>
-      <b-navbar-nav>
-        <b-nav-item to="/">Home</b-nav-item>
-        <b-nav-item to="/main/products">Products</b-nav-item>
-        <b-nav-item to="/main/about">About</b-nav-item>
-        <b-nav-item to="/main/contact">Contact us</b-nav-item>
-      </b-navbar-nav>
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item to="/">Home</b-nav-item>
+          <b-nav-item to="/main/products">Products</b-nav-item>
+          <b-nav-item to="/main/about">About</b-nav-item>
+          <b-nav-item to="/main/contact">Contact us</b-nav-item>
+        </b-navbar-nav>
 
-      <!-- Right aligned nav items -->
-      <b-navbar-nav class="ml-auto">
-        <b-nav-item to="/main/cart">
-        Cart <b-badge variant="light" v-if="cartCount > 0">{{ cartCount }}</b-badge>
-        </b-nav-item>
-        <b-nav-item to="/Auth/login">Login</b-nav-item>
-        <b-nav-item-dropdown right>
-          <!-- Using 'button-content' slot -->
-          <template v-slot:button-content>
-            <em>User</em>
-          </template>
-          <b-dropdown-item href="#">Profile</b-dropdown-item>
-          <b-dropdown-item href="#">Sign Out</b-dropdown-item>
-        </b-nav-item-dropdown>
-      </b-navbar-nav>
-    </b-collapse>
-  </b-navbar>
-</div>
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto">
+          <b-nav-item to="/main/cart">
+            Cart
+            <b-badge variant="light" v-if="cartCount > 0">{{
+              cartCount
+            }}</b-badge>
+          </b-nav-item>
+          <b-nav-item to="/Auth/login" v-if="!this.$auth.loggedIn"
+            >Login</b-nav-item
+          >
+                    <b-nav-item to="/Auth/register" v-if="!this.$auth.loggedIn"
+            >Register</b-nav-item
+          >
+          <b-nav-item-dropdown right v-if="this.$auth.loggedIn">
+            <!-- Using 'button-content' slot -->
+            <template v-slot:button-content>
+              <em>User</em>
+            </template>
+            <b-dropdown-item href="#">Profile</b-dropdown-item>
+            <b-dropdown-item @click="logout">Sign Out</b-dropdown-item>
+          </b-nav-item-dropdown>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </div>
 </template>
 
 <script>
-  import { mapActions, mapGetters } from 'vuex'
+import { mapActions, mapGetters } from 'vuex'
 
-  export default {
-    data: () => ({
-      links: [
+export default {
+  methods: {
+    async logout() {
+      await this.$auth.logout()
+      this.$router.push({ path: '/' })
+    },
+  },
+  data: () => ({
+    links: [
       { name: 'หน้าแรก', Link: '/main' },
       { name: 'สินค้า', Link: '/main/products' },
       { name: 'เกี่ยวกับเรา', Link: '/main/about' },
       { name: 'ติดต่อเรา', Link: '/main/contact' },
-      { name: 'ตะกร้าสินค้า', Link: '/main/cart' }
-    ]
-    }),
-    computed: {
-    ...mapGetters(["cartCount"])
-    },
-  }
+      { name: 'ตะกร้าสินค้า', Link: '/main/cart' },
+    ],
+  }),
+  computed: {
+    ...mapGetters(['cartCount']),
+  },
+}
 </script>
--- pages/Auth/login.vue
+++ pages/Auth/login.vue
@@ -1,17 +1,33 @@
 <template>
-  <v-main>
+  <v-main v-if="!this.$auth.loggedIn">
     <v-container fluid fill-height>
       <v-layout align-center justify-center>
         <v-flex xs12 sm8 md4>
-          <material-card
-            color="success"
-            elevation="12"
-            title="เข้าสู่ระบบ"
+          <v-alert v-if="loginsuccess === true" type="success"
+            >เข้าสู่ระบบสำเร็จ</v-alert
           >
+          <v-alert v-if="!loginsuccess === false" type="error"
+            >เข้าสู่ระบบไม่สำเร็จ : {{ error }}</v-alert
+          >
+          <material-card color="success" elevation="12" title="เข้าสู่ระบบ">
             <v-card-text>
               <v-form>
-                <v-text-field type="email" v-model="email" prepend-icon="mdi-account" name="email" label="Login"></v-text-field>
-                <v-text-field type="password" v-model="password" prepend-icon="mdi-lock" name="password" label="Password"></v-text-field>
+                <v-text-field
+                  type="email"
+                  v-model="email"
+                  :rules="emailRules"
+                  prepend-icon="mdi-account"
+                  name="email"
+                  label="Login"
+                ></v-text-field>
+                <v-text-field
+                  type="password"
+                  v-model="password"
+                  :rules="passwordRules"
+                  prepend-icon="mdi-lock"
+                  name="password"
+                  label="Password"
+                ></v-text-field>
               </v-form>
             </v-card-text>
             <v-card-actions>
@@ -27,38 +43,61 @@
 </template>
 
 <script>
- import { mapGetters } from 'vuex'
-  import materialCard  from '~/components/material/AppCard'
+import { mapGetters } from 'vuex'
+import materialCard from '~/components/material/AppCard'
+import Notification from '~/components/Notification'
 
-  export default {
-      computed: {
-    ...mapGetters(['isAuthenticated', 'loggedInUser'])
+export default {
+  // middleware: 'loggedin',
+  computed: {
+    ...mapGetters(['isAuthenticated', 'loggedInUser']),
   },
-    layout:'auth',
-    components: {
-      materialCard
-    },
-    data() {
+  layout: 'auth',
+  components: {
+    materialCard,
+    Notification,
+  },
+
+  data() {
     return {
       email: '',
-      password: ''
+      password: '',
+      loginsuccess: '',
+      error: null,
+      passwordRules: [(v) => !!v || 'จำเป็นต้องใส่ Password'],
+      emailRules: [
+        (v) => !!v || 'จำเป็นต้องใส่ E-mail',
+        (v) => /.+@.+\..+/.test(v) || 'รูปแบบบ E-mail ไม่ถูกต้อง',
+      ],
     }
   },
   methods: {
     async handleLoginClicked() {
       try {
         const response = await this.$auth.loginWith('local', {
-          data: { email: this.email, password: this.password  }
+          data: { email: this.email, password: this.password },
         })
-        console.log(response)
-        if (response.data.success) {
-          this.$router.push({ path: '/admin/dashboard' })
+       // console.log(response)
+        if (response.success) {
+          this.loginsuccess = response.success
+          //this.$router.push({ path: '/admin/dashboard' })
         }
       } catch (err) {
-        console.log(err)
+        this.loginsuccess = err
+        if ((this.error = err.response.data.success === false)) {
+          this.error = err.response.data.message
+        } else {
+          this.error = err.response.data
+        }
+        //console.log(err.response.data.success)
       }
+    },
+  },
+  mounted() {
+    if (this.$auth.loggedIn) {
+      this.$router.push({ path: '/' })
     }
-  }
-  }
+  },
+}
 </script>
 
